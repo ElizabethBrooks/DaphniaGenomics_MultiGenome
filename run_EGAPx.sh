64569b71e53ff0ec455ad994b3b35b77d4b767ef
ElizabethBrooks <merimaat1@gmail.com>
2024-06-27 01:55:25 -0400

    Testing EGAPx pipeline

--- run_EGAPx.sh
+++ run_EGAPx.sh
@@ -7,7 +7,8 @@
 # script to run the EGAPx pipeline
 
 # run EGAPx to copy config files
-python3 /afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/egapx.py /afs/crc.nd.edu/group/hoth/eBrooks/DaphniaGenomics_MultiGenome/inputs_KAP4_EGAPx.yaml -e singularity -w /scratch365/ebrooks5/multi_genome_project/temp_datapath_run0 -o /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0
+#python3 /afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/egapx.py /afs/crc.nd.edu/group/hoth/eBrooks/DaphniaGenomics_MultiGenome/inputs_KAP4_EGAPx.yaml -e singularity -w /scratch365/ebrooks5/multi_genome_project/temp_datapath_run0 -o /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0
 
 # run EGAPx
-python3 /afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/egapx.py /afs/crc.nd.edu/group/hoth/eBrooks/DaphniaGenomics_MultiGenome/inputs_KAP4_EGAPx.yaml -e singularity -w /scratch365/ebrooks5/multi_genome_project/temp_datapath_run0 -o /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0
+#python3 /afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/egapx.py /afs/crc.nd.edu/group/hoth/eBrooks/DaphniaGenomics_MultiGenome/inputs_KAP4_EGAPx.yaml -e singularity -w /scratch365/ebrooks5/multi_genome_project/temp_datapath_run0 -o /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0
+nextflow -C /afs/crc.nd.edu/group/hoth/eBrooks/DaphniaGenomics_MultiGenome/egapx_config/singularity.config,/afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/assets/config/default.config,/afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/assets/config/docker_image.config,/afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/assets/config/process_resources.config -log /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0/nextflow.log run /afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/../nf/ui.nf --output /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0 -with-report /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0/run.report.html -with-timeline /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0/run.timeline.html -with-trace /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0/run.trace.txt -params-file /scratch365/ebrooks5/multi_genome_project/D_pulex_KAP4_run0/run_params.yaml -resume
