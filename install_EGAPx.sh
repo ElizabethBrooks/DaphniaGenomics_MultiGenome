b391f404a0329de80bed3e56a4c251fdd4e96167
ElizabethBrooks <merimaat1@gmail.com>
2024-06-27 02:05:45 -0400

    Testing EGAPx pipeline inputs

--- install_EGAPx.sh
+++ install_EGAPx.sh
@@ -12,10 +12,10 @@ cd egapx
 # Create a virtual environment where you can run EGAPx. There is a requirements.txt file. PyYAML will be installed in this environment
 python -m venv /afs/crc.nd.edu/user/e/ebrooks5/EGAPx_env
 source /afs/crc.nd.edu/user/e/ebrooks5/EGAPx_env/bin/activate
-pip install -r ui/requirements.txt
+pip install -r /afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/requirements.txt
 
 # Run EGAPx for the first time to copy the config files so you can edit them
-python3 ui/egapx.py ./examples/input_D_farinae_small.yaml -o example_out 
+python3 /afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/egapx.py /afs/crc.nd.edu/user/e/ebrooks5/egapx/examples/input_D_farinae_small.yaml -o example_out 
 
 # Run EGAPx with the following command for real this time
-python3 ui/egapx.py ./examples/input_D_farinae_small.yaml -e singularity -w /afs/crc.nd.edu/user/e/ebrooks5/egapx_temp_datapath/D_farinae -o example_out
+python3 /afs/crc.nd.edu/user/e/ebrooks5/egapx/ui/egapx.py /afs/crc.nd.edu/user/e/ebrooks5/egapx/examples/input_D_farinae_small.yaml -e singularity -w /afs/crc.nd.edu/user/e/ebrooks5/egapx_temp_datapath/D_farinae -o example_out
