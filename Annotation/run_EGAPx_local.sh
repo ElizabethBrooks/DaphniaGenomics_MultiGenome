8d69a5f0628596611362d4fcd519bbeb71c2f604
ElizabethBrooks <merimaat1@gmail.com>
2024-06-29 18:12:57 -0400

    Updated input paths

--- Annotation/run_EGAPx_local.sh
+++ Annotation/run_EGAPx_local.sh
@@ -6,6 +6,8 @@
 # usage ex: bash run_EGAPx_local.sh inputs_KAP106.txt
 # usage ex: bash run_EGAPx_local.sh inputs_LK16.txt
 # usage ex: bash run_EGAPx_local.sh inputs_LK16_NCBI.txt
+# usage ex: bash run_EGAPx_HPC.sh inputs_LK16_raw.txt
+# usage ex: bash run_EGAPx_HPC.sh inputs_LK16_trimmed.txt
 
 # retrieve input file
 inputFile=$1
