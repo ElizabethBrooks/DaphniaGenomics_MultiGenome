b97f0e97f3d328472e3c31f1221cbd29c35913a3
ElizabethBrooks <merimaat1@gmail.com>
2024-10-30 23:43:05 -0400

    Moved testing notes

--- annotation/run_EGAPx_HPC.sh
+++ annotation/run_EGAPx_HPC.sh
@@ -7,20 +7,6 @@
 
 # script to run the EGAPx pipeline
 # usage: qsub run_EGAPx_HPC.sh inputFile
-# usage ex: qsub run_EGAPx_HPC.sh inputs_KAP4_NCBI.txt
-## job 694733 -> DONE -> ERROR ~ Error executing process > 'egapx:star:exec (111)'
-##  Wallclock Time   = 10:23:29:22
-# usage ex: qsub run_EGAPx_HPC.sh inputs_KAP106.txt
-## job 699252 -> DONE -> ERROR ~ Error executing process > 'egapx:star:exec (107)'
-## Wallclock Time   = 8:15:36:18
-# usage ex: qsub run_EGAPx_HPC.sh inputs_LK16_NCBI.txt
-## job 773663 -> KILLED -> Error: (CSQLITE_Exception::eStmtStep) Error from sqlite3: [778] disk I/O error
-# usage ex: qsub run_EGAPx_HPC.sh inputs_SC_F0-13Bv2.txt
-## job 773672 -> DONE -> curl: (52) Empty reply from server
-## Wallclock Time   = 4:16:23:22
-## Process `egapx:fetch_sra_fasta:run_fetch_sra_fasta (69)` terminated with an error exit status (52)
-# usage ex: qsub run_EGAPx_HPC.sh inputs_NIES_NCBI.txt
-## job 773796 -> KILLED -> ERROR ~ Error executing process > 'egapx:run_get_hmm'
 
 # NOTE: the default /egapx/ui/assets/config/process_resources.config file specifies up to 31 cores (huge_Job)
 # our afs system has 263Gb RAM, 64 cores
