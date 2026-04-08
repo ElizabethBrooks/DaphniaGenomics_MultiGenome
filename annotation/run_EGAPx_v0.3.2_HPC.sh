3229fa2a3aabf894c62d3ea2be341d3d0559880e
ElizabethBrooks <merimaat1@gmail.com>
2026-04-08 16:31:27 -0400

    Testing local offline mode steps and inputs

--- annotation/run_EGAPx_v0.3.2_HPC.sh
+++ annotation/run_EGAPx_v0.3.2_HPC.sh
@@ -8,9 +8,10 @@
 # script to run the EGAPx v0.3.2 pipeline
 # usage: qsub run_EGAPx_v0.3.2_HPC.sh inputFile
 # usage ex: qsub run_EGAPx_v0.3.2_HPC.sh EGAPx_v0.3.2/D_melanica/inputs_CON6_BC.txt
-## job 793353 -> NOTE: Nextflow is not tested with Java 1.8.0_462 -- It's recommended the use of version 11 up to 23
-## job-test 793355 -> openjdk version "1.8.0_462"
-## job -> ImportError: /afs/crc.nd.edu/x86_64_linux/b/bio/0724/lib/python3.12/lib-dynload/_sqlite3.cpython-312-x86_64-linux-gnu.so: undefined symbol: sqlite3_deserialize
+## job (first test) -> NOTE: Nextflow is not tested with Java 1.8.0_462 -- It's recommended the use of version 11 up to 23
+## job (java -version) -> openjdk version "1.8.0_462"
+## job (module load bio) -> ImportError: /afs/crc.nd.edu/x86_64_linux/b/bio/0724/lib/python3.12/lib-dynload/_sqlite3.cpython-312-x86_64-linux-gnu.so: undefined symbol: sqlite3_deserialize
+## job (module load java/25.0.1)
 
 # NOTE: the default /egapx/ui/assets/config/process_resources.config file specifies up to 31 cores (huge_Job)
 # our afs system has 263Gb RAM, 64 cores
@@ -18,7 +19,7 @@
 # smaller annotation jobs do not need to request as many resources (for example, 15 cores is sufficient for the average run)
 
 # load module with updated java version
-#module load bio
+module load java/25.0.1
 
 # retrieve input file
 inputFile=$1
@@ -67,31 +68,29 @@ echo "Beginning analysis of $speciesName..."
 # the normal workflow uses remote NCBI data, which has been giving errors
 # https://github.com/ncbi/egapx/issues/214
 # run EGAPx script to download necessary data for local running
-python3 $softwarePath"/ui/egapx.py" $inputsPath -dl -lc $outputsPath"/local_cache"
+#python3 $softwarePath"/ui/egapx.py" $inputsPath -dl -lc $outputsPath"/local_cache"
 
 # run EGAPx to copy config files
 # test: 
 # python3 /afs/crc.nd.edu/user/e/ebrooks5/egapx_v0.3.2/egapx/ui/egapx.py /afs/crc.nd.edu/group/hoth/eBrooks/DaphniaGenomics_MultiGenome/inputData/EGAPx_v0.3.2/D_melanica/inputs_CON6_ZQ_MP.yaml -e singularity -w /scratch365/ebrooks5/multi_genome_project/EGAPx_v0.3.2/tested_April2026/BC_tests/D_melanica_CON6_ZQ_MP/temp_datapath -o /scratch365/ebrooks5/multi_genome_project/EGAPx_v0.3.2/tested_April2026/BC_tests/D_melanica_CON6_ZQ_MP -lc /scratch365/ebrooks5/multi_genome_project/EGAPx_v0.3.2/tested_April2026/BC_tests/D_melanica_CON6_ZQ_MP/local_cache
-#python3 $softwarePath"/ui/egapx.py" $inputsPath -o $outputsPath
-#python3 $softwarePath"/ui/egapx.py" $inputsPath -e singularity -w $outputsPath"/temp_datapath" -o $outputsPath -lc $outputsPath"/local_cache"
+python3 $softwarePath"/ui/egapx.py" $inputsPath -e singularity -w $outputsPath"/temp_datapath" -o $outputsPath -lc $outputsPath"/local_cache"
 
-# copy the updated config files
-# test: 
-# first, updated cache dir path (/scratch365/ebrooks5/egapx_v0.3.2/data/singularity) in the biowulf_cluster.config, biowulf_local.config, and slurm.config files
-# next, updated tmp dir path (/scratch365/ebrooks5/egapx_v0.3.2/data/tmp) in the slurm.config file
-# cp -r /afs/crc.nd.edu/user/e/ebrooks5/egapx_v0.3.2/egapx/egapx_config .
-#cp -r $softwarePath"/egapx_config" .
+## copy the updated config files
+## test: 
+## first, updated cache dir path (/scratch365/ebrooks5/egapx_v0.3.2/data/singularity) in the biowulf_cluster.config, biowulf_local.config, and slurm.config files
+## next, updated tmp dir path (/scratch365/ebrooks5/egapx_v0.3.2/data/tmp) in the slurm.config file
+## cp -r /afs/crc.nd.edu/user/e/ebrooks5/egapx_v0.3.2/egapx/egapx_config .
+##cp -r $softwarePath"/egapx_config" .
 
-# update the process container path (if using the biowulf_cluster, but that appears to be for slurm machines)
-# test:
-#echo "process.container = '/scratch365/ebrooks5/multi_genome_project/EGAPx_v0.3.2/tested_April2026/BC_tests/D_melanica_CON6_ZQ_MP/temp_datapath/singularity/ncbi-egapx-0.3.2-alpha.img'"  >> egapx_config/biowulf_cluster.config
-#echo "process.container = '/scratch365/ebrooks5/egapx_v0.3.2/data/singularity/ncbi-egapx-0.3.2-alpha.img'"  >> egapx_config/biowulf_cluster.config
+## update the process container path (if using the biowulf_cluster, but that appears to be for slurm machines)
+## test:
+##echo "process.container = '/scratch365/ebrooks5/multi_genome_project/EGAPx_v0.3.2/tested_April2026/BC_tests/D_melanica_CON6_ZQ_MP/temp_datapath/singularity/ncbi-egapx-0.3.2-alpha.img'"  >> egapx_config/biowulf_cluster.config
+##echo "process.container = '/scratch365/ebrooks5/egapx_v0.3.2/data/singularity/ncbi-egapx-0.3.2-alpha.img'"  >> egapx_config/biowulf_cluster.config
 
 # run EGAPx
 # test:
 # python3 /afs/crc.nd.edu/user/e/ebrooks5/egapx_v0.3.2/egapx/ui/egapx.py /afs/crc.nd.edu/group/hoth/eBrooks/DaphniaGenomics_MultiGenome/inputData/EGAPx_v0.3.2/D_melanica/inputs_CON6_ZQ_MP.yaml -e singularity -w /scratch365/ebrooks5/multi_genome_project/EGAPx_v0.3.2/tested_April2026/BC_tests/D_melanica_CON6_ZQ_MP/temp_datapath -o /scratch365/ebrooks5/multi_genome_project/EGAPx_v0.3.2/tested_April2026/BC_tests/D_melanica_CON6_ZQ_MP -lc /scratch365/ebrooks5/multi_genome_project/EGAPx_v0.3.2/tested_April2026/BC_tests/D_melanica_CON6_ZQ_MP/local_cache
-#python3 $softwarePath"/ui/egapx.py" $inputsPath -e singularity -w $outputsPath"/temp_datapath" -o $outputsPath -lc $outputsPath"/local_cache"
-#python3 $softwarePath"/ui/egapx.py" $inputsPath -e singularity -w $outputsPath"/temp_datapath" -o $outputsPath -lc $outputsPath"/local_cache"
+python3 $softwarePath"/ui/egapx.py" $inputsPath -e singularity -w $outputsPath"/temp_datapath" -o $outputsPath -lc $outputsPath"/local_cache"
 
 # uncomment the following lines to reduce data storage
 # clean up, if accept.gff output file exsists
