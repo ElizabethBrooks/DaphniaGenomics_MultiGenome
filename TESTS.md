c229e91b306d8806256bd4f90a314746ce4edfc6
ElizabethBrooks <merimaat1@gmail.com>
2026-05-05 12:36:45 -0400

    Updating formatting scripts and notes

--- TESTS.md
+++ TESTS.md
@@ -1,5 +1,24 @@
 # Analysis Tests
 
+# Cufflinks convert gff - May 2026 <- EGAPx v0.3.2 outputs
+
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_melanica/inputs_CON6_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_salina/inputs_ULW_001_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/Ceriodaphnia_sp/inputs_CUH_004_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_similis/inputs_BGL_001_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_pulex/inputs_LKG_002_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_pulicaria/inputs_ESL_002_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_pulicaria/inputs_TF22_005_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_magna/inputs_MCL_001_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_mediterranea/inputs_CY_ORK_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_schodleri/inputs_Mary_Lake_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_retrocurva/inputs_BLK_001_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_tenebrosa/inputs_C31_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_mendotae/inputs_67B_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/Latona_sp/inputs_BLK_101_BC_clean.txt
+qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_magna/inputs_LRVO_BC_clean.txt
+
+
 # OrthoFinder orthos - May 2026 <- AGAT outputs
 
 ## all
---
+++ formatting/convert_gff_gtf_cufflinks.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#$ -M ebrooks5@nd.edu
+#$ -m abe
+#$ -r n
+#$ -N convert_gff_cufflinks_jobOutput
+
+# script to keep only the longest isoforms in the input gff
+# usage: qsub convert_gff_gtf_cufflinks.sh inputFile
+# usage ex: qsub convert_gff_gtf_cufflinks.sh EGAPx_v0.3.2/D_melanica/inputs_CON6_BC_clean.txt
+
+# load software
+module load bio/2.0
+
+# retrieve input file
+inputFile=$1
+
+# retrieve species name
+speciesName=$(grep "species:" ../"inputData/"$inputFile | tr -d " " | sed "s/species://g")
+
+# retrieve inputs path
+inputsPath=$(grep "inputs_EGAPx:" ../"inputData/"$inputFile | tr -d " " | sed "s/inputs_EGAPx://g")
+
+# retrieve repository directory
+repoDir=$(dirname $PWD)
+
+# setup inputs path
+inputsPath=$repoDir"/inputData/"$inputsPath
+
+# retrieve software path
+softwarePath=$(grep "software_AGAT:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/software_AGAT://g")
+
+# retrieve outputs path
+# change this for different test runs
+outputsPath=$(grep "outputs_EGAPx_v0.3.2_BC:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_BC://g")
+
+# setup outputs path
+outputsPath=$outputsPath"/"$speciesName
+
+# create outputs directory
+mkdir $outputsPath"/AGAT_v1.4.2/Cufflinks_v2.2.1"
+
+# move to the AGAT software directory
+cd $outputsPath"/AGAT_v1.4.2/Cufflinks_v2.2.1"
+
+# status message
+echo "Beginning analysis of $speciesName..."
+
+# extract longest proteins
+gffread $outputsPath"/AGAT_v1.4.2/output_longest.gff" -F -T -o $outputsPath"/AGAT_v1.4.2/Cufflinks_v2.2.1/output_longest.gtf"
+
+# status message
+echo "Analysis of $speciesName complete!"
