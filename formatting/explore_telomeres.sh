3e41a8f278d12dca9f0d9cc7acc68a07dac3132e
ElizabethBrooks <merimaat1@gmail.com>
2026-07-09 13:57:27 -0400

    Added telomere test

--- formatting/explore_telomeres.sh
+++ formatting/explore_telomeres.sh
@@ -51,7 +51,7 @@ cd $outputsPath"/tidk_0.2.65/length_6_test"
 echo "Beginning analysis of $speciesName..."
 
 # explore for telomeric repeats
-tidk explore --length 6 --minimum 5 --maximum 12 $inputsDir"/complete.genomic.fna" > $outputsPath"/tidk_0.2.65/telomeric_repeat.tsv"
+tidk explore --length 6 --minimum 6 --maximum 12 $inputsDir"/complete.genomic.fna" > $outputsPath"/tidk_0.2.65/telomeric_repeat.tsv"
 
 # status message
 echo "Analysis of $speciesName complete!"
