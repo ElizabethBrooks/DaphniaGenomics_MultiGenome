3a49090e64f21eb89ccf3894a8fce5796fbb63d3
ElizabethBrooks <merimaat1@gmail.com>
2026-07-09 02:26:20 -0400

    Adding scripts to find telomere seqs

---
+++ formatting/extract_telomeres.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#$ -M ebrooks5@nd.edu
+#$ -m abe
+#$ -r n
+#$ -N extract_telomeres_jobOutput
+
+# script to extract telomere sequences
+# usage: qsub extract_telomeres.sh inputFile
+
+# load software
+conda activate my_tidk
+
+# retrieve input file
+inputFile=$1
+
+# retrieve species name
+speciesName=$(grep "species:" ../"inputData/"$inputFile | tr -d " " | sed "s/species://g")
+
+# retrieve inputs path
+inputsPath=$(grep "inputs_EGAPx:" ../"inputData/"$inputFile | tr -d " " | sed "s/inputs_EGAPx://g")
+
+# retrieve repository directory
+repoDir=$(dirname $PWD)
+
+# setup inputs path
+inputsPath=$repoDir"/inputData/"$inputsPath
+
+# retrieve software path
+#softwarePath=$(grep "software_AGAT:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/software_AGAT://g")
+softwarePath=$(grep "software_AGAT_new:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/software_AGAT_new://g")
+
+# retrieve inputs directory
+# change this for different test runs
+#inputsDir=$(grep "outputs_EGAPx_v0.3.2_ZQ_V2:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_ZQ_V2://g")
+#inputsDir=$(grep "outputs_EGAPx_v0.3.2_NCBI:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_NCBI://g")
+#inputsDir=$(grep "outputs_EGAPx_v0.3.2_zenodo:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_zenodo://g")
+#inputsDir=$(grep "outputs_EGAPx_v0.3.2_CNGBdb:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_CNGBdb://g")
+#inputsDir=$(grep "outputs_EGAPx_v0.3.2_ZQ_B1:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_ZQ_B1://g")
+#inputsDir=$(grep "outputs_EGAPx_v0.3.2_ZQ_B2:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_ZQ_B2://g")
+inputsDir=$(grep "outputs_EGAPx_v0.3.2_BC:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_BC://g")
+
+# setup inputs directory path
+inputsDir=$inputsDir"/"$speciesName
+
+# retrieve outputs path
+outputsPath=$(grep "outputs_EGAPx_v0.3.2_telomeres:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_telomeres://g")
+
+# create outputs directory
+mkdir $outputsPath
+mkdir $outputsPath"/tidk_0.2.65"
+
+# move to the outputs directory
+cd $outputsPath"/tidk_0.2.65"
+
+# status message
+echo "Beginning analysis of $speciesName..."
+
+# extract telomere sequences
+tidk search -s $outputsPath"/tidk_0.2.65/telomeric_repeat.tsv" -f $inputsDir"/complete.genomic.fna" -o "telomere_sequences" -d $outputsPath"/tidk_0.2.65"
+
+# status message
+echo "Analysis of $speciesName complete!"
