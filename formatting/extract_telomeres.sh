b4830458871c7f8f841cc247619dae04dc4e228d
ElizabethBrooks <merimaat1@gmail.com>
2026-07-11 13:43:46 -0400

    Updated TRM search window

--- formatting/extract_telomeres.sh
+++ formatting/extract_telomeres.sh
@@ -23,10 +23,10 @@ inputsPath=$repoDir"/inputData/"$inputsPath
 
 # retrieve inputs directory
 # change this for different test runs
-#inputsDir=$(grep "outputs_EGAPx_v0.3.2_BC:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_BC://g")
+inputsDir=$(grep "outputs_EGAPx_v0.3.2_BC:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_BC://g")
 #inputsDir=$(grep "outputs_EGAPx_v0.3.2_CNGBdb:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_CNGBdb://g")
 #inputsDir=$(grep "outputs_EGAPx_v0.3.2_zenodo:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_zenodo://g")
-inputsDir=$(grep "outputs_EGAPx_v0.3.2_NCBI:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_NCBI://g")
+#inputsDir=$(grep "outputs_EGAPx_v0.3.2_NCBI:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_NCBI://g")
 #inputsDir=$(grep "outputs_EGAPx_v0.3.2_ZQ_V2:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_ZQ_V2://g")
 #inputsDir=$(grep "outputs_EGAPx_v0.3.2_ZQ_B2:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_ZQ_B2://g")
 #inputsDir=$(grep "outputs_EGAPx_v0.3.2_ZQ_B1:" ../"inputData/inputs_annotations.txt" | tr -d " " | sed "s/outputs_EGAPx_v0.3.2_ZQ_B1://g")
@@ -49,12 +49,12 @@ cd $outputsPath"/tidk_0.2.65"
 echo "Beginning analysis of $speciesName..."
 
 # retrieve most abundant telomeric repeat
-#telomere1=$(head -2 $outputsPath"/tidk_0.2.65/telomeric_repeat.tsv" | tail -1 | cut -f1)
+telomere1=$(head -2 $outputsPath"/tidk_0.2.65/telomeric_repeat.tsv" | tail -1 | cut -f1)
 telomere2="AACCT"
 telomere3="AACCTAACCT"
 
 # extract telomere sequences
-#tidk search -s $telomere1 -o "most_common" -d $outputsPath"/tidk_0.2.65" $inputsDir"/complete.genomic.fna"
+tidk search -s $telomere1 -o "most_common" -d $outputsPath"/tidk_0.2.65" $inputsDir"/complete.genomic.fna"
 tidk search -s $telomere2 -o "AACCT" -d $outputsPath"/tidk_0.2.65" $inputsDir"/complete.genomic.fna"
 tidk search -s $telomere3 -o "AACCTAACCT" -d $outputsPath"/tidk_0.2.65" $inputsDir"/complete.genomic.fna"
 #tidk search -s $telomere1 -o "most_common" -d $outputsPath"/tidk_0.2.65" /groups/mpfrende/Projects_2026/multi_genome/data/NCBI_genomes/Artemia_franciscana.fna
