006542688330f884e4ab735d69529a611d128b87
ElizabethBrooks <merimaat1@gmail.com>
2026-07-23 12:51:52 -0400

    Updated feature extraction

--- formatting/extract_features_AGAT.sh
+++ formatting/extract_features_AGAT.sh
@@ -54,7 +54,6 @@ echo "Beginning analysis of $speciesName..."
 
 # pre clean
 #rm $outputsPath"/AGAT_v1.4.2/lncRNAs.fa"
-rm $outputsPath"/AGAT_v1.4.2/lncRNAs_merged.fa"
 #rm $outputsPath"/AGAT_v1.4.2/five_prime_utrs_merged.fa"
 #rm $outputsPath"/AGAT_v1.4.2/three_prime_utrs_merged.fa"
 #rm $outputsPath"/AGAT_v1.4.2/exons_merged.fa"
@@ -63,14 +62,14 @@ rm $outputsPath"/AGAT_v1.4.2/lncRNAs_merged.fa"
 # extract lnc RNAs
 #agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type lnc_RNA --output $outputsPath"/AGAT_v1.4.2/lncRNAs.fa"
 #agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type lncRNA --output $outputsPath"/AGAT_v1.4.2/lncRNAs.fa"
-
-# extract merged lnc RNAs
-agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type lnc_RNA --merge --output $outputsPath"/AGAT_v1.4.2/lncRNAs_merged.fa"
-#agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type lncRNA --merge --output $outputsPath"/AGAT_v1.4.2/lncRNAs_merged.fa"
+agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type lnc_RNA --keep_parent_attributes --output $outputsPath"/AGAT_v1.4.2/lncRNAs_att.fa"
+agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type lncRNA --keep_parent_attributes --output $outputsPath"/AGAT_v1.4.2/lncRNAs_att.fa"
 
 # extract merged UTRs
 #agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type five_prime_UTR --merge --output $outputsPath"/AGAT_v1.4.2/five_prime_utrs_merged.fa"
 #agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type three_prime_UTR --merge --output $outputsPath"/AGAT_v1.4.2/three_prime_utrs_merged.fa"
+agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type five_prime_UTR --merge --keep_parent_attributes --output $outputsPath"/AGAT_v1.4.2/five_prime_utrs_merged_att.fa"
+agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type three_prime_UTR --merge --keep_parent_attributes --output $outputsPath"/AGAT_v1.4.2/three_prime_utrs_merged_att.fa"
 
 # extract meregd exons
 #agat_sp_extract_sequences.pl --gff $outputsPath"/complete.genomic.gff" --fasta $outputsPath"/complete.genomic.fna" --type exon --merge --keep_parent_attributes --output $outputsPath"/AGAT_v1.4.2/exons_merged.fa"
